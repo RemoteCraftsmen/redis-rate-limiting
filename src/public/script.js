e7b3f073d66420dd03c389743c4d6873ce7b1728
Rafal Ksiazek <rafal.ksiazek@remotecraftsmen.com>
2020-12-08 22:42:28 +0100

    2 parallel connection in Safari

--- src/public/script.js
+++ src/public/script.js
@@ -54,8 +54,7 @@ const onSendButtonClick = async e => {
                 headers: {
                     'Cache-Control': 'no-cache, no-store, must-revalidate',
                     'Pragma': 'no-cache',
-                    'Expires': '0',
-                    'Vary': '*'
+                    'Expires': '0'
                 }
             });
 
@@ -65,7 +64,7 @@ const onSendButtonClick = async e => {
         }
     };
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 2; i++) {
         calls.push(callPing());
     }
 
